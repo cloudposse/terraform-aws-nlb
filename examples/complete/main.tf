a8d51898c6b9a9a2a375d0412d90dc1c5b95018d
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2021-04-22 01:01:58 +0000

    Auto Format

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -12,9 +12,9 @@ module "vpc" {
 }
 
 module "subnets" {
-  source               = "cloudposse/dynamic-subnets/aws"
-  version              = "0.39.0"
-  
+  source  = "cloudposse/dynamic-subnets/aws"
+  version = "0.39.0"
+
   availability_zones   = var.availability_zones
   vpc_id               = module.vpc.vpc_id
   igw_id               = module.vpc.igw_id
