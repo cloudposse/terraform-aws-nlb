9e6b78e071a4f88eb9a3ce4ca083108fe2d42bff
Josh Balduff <11274822+joshmello@users.noreply.github.com>
2022-07-28 02:11:55 +0000

    updating log bucket version

--- main.tf
+++ main.tf
@@ -9,11 +9,18 @@ locals {
 
 module "access_logs" {
   source  = "cloudposse/lb-s3-bucket/aws"
-  version = "0.12.0"
+  version = "0.16.0"
 
-  enabled                            = module.this.enabled && var.access_logs_enabled && var.access_logs_s3_bucket_id == null
-
+  enabled = module.this.enabled && var.access_logs_enabled && var.access_logs_s3_bucket_id == null
+  
+  attributes = compact(concat(module.this.attributes, ["nlb", "access", "logs"]))
+  
   allow_ssl_requests_only            = var.allow_ssl_requests_only
+  force_destroy                 = var.nlb_access_logs_s3_bucket_force_destroy
+  force_destroy_enabled         = var.nlb_access_logs_s3_bucket_force_destroy_enabled
+  lifecycle_configuration_rules = var.lifecycle_configuration_rules
+
+  # TODO: deprecate these inputs in favor of `lifecycle_configuration_rules`
   lifecycle_rule_enabled             = var.lifecycle_rule_enabled
   enable_glacier_transition          = var.enable_glacier_transition
   expiration_days                    = var.expiration_days
@@ -21,7 +28,6 @@ module "access_logs" {
   noncurrent_version_expiration_days = var.noncurrent_version_expiration_days
   noncurrent_version_transition_days = var.noncurrent_version_transition_days
   standard_transition_days           = var.standard_transition_days
-  force_destroy                      = var.nlb_access_logs_s3_bucket_force_destroy
 
   context = module.this.context
 }
