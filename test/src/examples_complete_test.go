bf75093c580e44164f94f40fcb58954132a2eb77
Igor Rodionov <goruha@gmail.com>
2022-12-05 21:32:00 +0300

    Apply changes from #23 PR

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -32,35 +32,35 @@ func TestExamplesComplete(t *testing.T) {
 
 	// We need that to follow 'force_destroy' pattern
 	// https://github.com/cloudposse/terraform-aws-s3-log-storage/wiki/Upgrading-to-v0.28.0-and-AWS-provider-v4-(POTENTIAL-DATA-LOSS)#the-safe-way
-	terraformOptionsWithDestroy := &terraform.Options{
-		// The path to where our Terraform code is located
-		TerraformDir: "../../examples/complete",
-		Upgrade:      true,
-		// Variables to pass to our Terraform code using -var-file options
-		VarFiles: []string{"fixtures.us-east-2.tfvars"},
-		Vars: map[string]interface{}{
-			"attributes": attributes,
-			"nlb_access_logs_s3_bucket_force_destroy": true,
-		},
-	}
+	//terraformOptionsWithDestroy := &terraform.Options{
+	//	// The path to where our Terraform code is located
+	//	TerraformDir: "../../examples/complete",
+	//	Upgrade:      true,
+	//	// Variables to pass to our Terraform code using -var-file options
+	//	VarFiles: []string{"fixtures.us-east-2.tfvars"},
+	//	Vars: map[string]interface{}{
+	//		"attributes": attributes,
+	//		"nlb_access_logs_s3_bucket_force_destroy": true,
+	//	},
+	//}
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer terraform.Destroy(t, terraformOptionsWithDestroy)
+	defer terraform.Destroy(t, terraformOptions)
 
-	terraform.Apply(t, terraformOptionsWithDestroy)
+	// terraform.Apply(t, terraformOptions)
 
 	// Run `terraform output` to get the value of an output variable
-	vpcCidr := terraform.Output(t, terraformOptionsWithDestroy, "vpc_cidr")
+	vpcCidr := terraform.Output(t, terraformOptions, "vpc_cidr")
 	// Verify we're getting back the outputs we expect
 	assert.Contains(t, "172.16.0.0/16", vpcCidr)
 
 	// Run `terraform output` to get the value of an output variable
-	privateSubnetCidrs := terraform.OutputList(t, terraformOptionsWithDestroy, "private_subnet_cidrs")
+	privateSubnetCidrs := terraform.OutputList(t, terraformOptions, "private_subnet_cidrs")
 	// Verify we're getting back the outputs we expect
 	assert.Equal(t, []string{"172.16.0.0/19", "172.16.32.0/19"}, privateSubnetCidrs)
 
 	// Run `terraform output` to get the value of an output variable
-	publicSubnetCidrs := terraform.OutputList(t, terraformOptionsWithDestroy, "public_subnet_cidrs")
+	publicSubnetCidrs := terraform.OutputList(t, terraformOptions, "public_subnet_cidrs")
 	// Verify we're getting back the outputs we expect
 	assert.Equal(t, []string{"172.16.96.0/19", "172.16.128.0/19"}, publicSubnetCidrs)
 
@@ -72,17 +72,17 @@ func TestExamplesComplete(t *testing.T) {
 	*/
 
 	// Run `terraform output` to get the value of an output variable
-	nlbName := terraform.Output(t, terraformOptionsWithDestroy, "nlb_name")
+	nlbName := terraform.Output(t, terraformOptions, "nlb_name")
 	// Verify we're getting back the outputs we expect
 	assert.Equal(t, "eg-test-nlb-" + randID, nlbName)
 
 	// Run `terraform output` to get the value of an output variable
-	defaultTargetGroupArn := terraform.Output(t, terraformOptionsWithDestroy, "default_target_group_arn")
+	defaultTargetGroupArn := terraform.Output(t, terraformOptions, "default_target_group_arn")
 	// Verify we're getting back the outputs we expect
 	assert.Contains(t, defaultTargetGroupArn, ":targetgroup/eg-test-nlb-" + randID + "-default")
 
 	// Run `terraform output` to get the value of an output variable
-	defaultListenerArn := terraform.Output(t, terraformOptionsWithDestroy, "default_listener_arn")
+	defaultListenerArn := terraform.Output(t, terraformOptions, "default_listener_arn")
 	// Verify we're getting back the outputs we expect
 	assert.Contains(t, defaultListenerArn, ":listener/net/eg-test-nlb-" + randID)
 }
