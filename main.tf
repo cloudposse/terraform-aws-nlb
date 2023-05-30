d5aac09da453915930facbaca09fe87bdb6ecad3
Jakub Rosa <jakub.rosa.95@gmail.com>
2023-05-30 20:24:10 +0200

    feat: add subnet_mapping and limit id lenght lb and tg (#34)
    * feat: add subnet_mapping and limit for id lb and tg
    
    * fix: add changes from auto-format jobs
    
    * feat: add additional certs for tls
    
    * fix: add health_check_unhealthy_threshold
    
    * fix: udapte readme
    
    * fix: run make auto format
    
    * fix: set unhealthy_threshold backward compatibility, fix tflint errors
    
    * fix: move locals variable in one place
    
    * fix: move locals definition
    
    * fix: terarform fmt

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 locals {
   listener_port         = var.tcp_enabled ? (var.udp_enabled ? var.udp_port : var.tcp_port) : var.udp_port
   listener_proto        = var.tcp_enabled ? (var.udp_enabled ? "TCP_UDP" : "TCP") : "UDP"
-  tcp_port              = var.udp_enabled ? var.udp_port : var.tcp_port
   health_check_port     = coalesce(var.health_check_port, "traffic-port")
   target_group_protocol = var.tls_enabled ? "TCP" : local.listener_proto
   health_check_protocol = coalesce(var.health_check_protocol, local.target_group_protocol)
